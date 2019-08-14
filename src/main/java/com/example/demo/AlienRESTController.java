d91376861a2a35e7390b02561e344383eb9ff504
z0031bha <z0031bha@md1tcpkc.ad001.siemens.net>
2019-08-14 14:32:08 +0530

    Put and delete updated

--- src/main/java/com/example/demo/AlienRESTController.java
+++ src/main/java/com/example/demo/AlienRESTController.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Alien;
@@ -26,6 +28,24 @@ public class AlienRESTController {
 		
 		return alien;
 	}
+	
+	@PutMapping(path="/alien")
+	public Alien updateAliens(@RequestBody Alien alien)
+	{
+		al.save(alien);
+		
+		return alien;
+	}
+	
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable("aid") int id)
+	{
+		Alien a = al.getOne(id);
+		
+		al.delete(a);
+		return "deleted";
+	}
+	
 	@GetMapping(path="/aliens")
 	public List<Alien> getAliens()
 	{
